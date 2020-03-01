6567a077006f0df0311a305841cdadfbe057023f
Lake Developer <lakeinvestmentsprime@gmail.com>
2020-03-02 00:18:49 +0200

    changed serverjs and .env

--- server.js
+++ server.js
@@ -10,17 +10,13 @@ app.prepare()
   .then(() => {
     const server = express()
 
-    server.get("/article/:id", (req, res) => {
-      const actualPage = "/article";
-      const queryParams = { id: req.params.id };
-      console.dir("req.params.id = " + JSON.stringify(req.params.id));
-      app.render(req, res, actualPage, queryParams);
+    server.get('*', (req, res) => {
+      return handle(req, res);
     });
 
-
-    server.listen(port, (err) => {
+    server.listen(port, err => {
       if (err) throw err;
-      console.log('>Ready on http://localhost:3000');
+      console.log(`Ready on ${port}`);
     })
   })
   .catch((ex) => {
