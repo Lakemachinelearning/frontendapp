54efc974dcd64602de53887b59b6f49d9c6a6789
Lake Developer <lakeinvestmentsprime@gmail.com>
2020-03-01 21:00:49 +0200

    added more

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.prepare()
     });
 
 
-    server.listen(3000, (err) => {
-      if (err) throw err
+    server.listen(process.env.PORT || 3000, err => {
+      if (err) throw err;
       console.log('>Ready on http://localhost:3000');
     })
   })
   .catch((ex) => {
     console.error(ex.stack)
     process.exit(1)
-  })
+  });
